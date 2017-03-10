d029fc700f8a0d3b3aeb0090398bf2d89d1c0c89
Melanie Yeoh <melanieyeoh@Melanies-MacBook-Air.local>
2017-03-10 18:27:24 +0800

    bootstrap profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,36 +7,41 @@ class UsersController < Clearance::UsersController
 	def edit
 		@user = User.find(params[:id])
 	end
- 
-  def update 
+
+  def update
   	@user = User.find(params[:id])
 	  	if @user.update(user_params)
-	  	@notice = "User updated!"
+	  		@notice = "User updated!"
 			else
-			@notice = "User update FAILED"
-			end 
+				@notice = "User update FAILED"
+			end
     redirect_to user_path(@user), :notice => @notice
   end
 
+	def new
+
+	end
+
+
 	def create
-	@user = User.new(user_params)
+		@user = User.new(user_params)
 		  if @user.save
-		  @notice = "User created!"
+		  	@notice = "User created!"
 			else
-			@notice = "User creation FAILED"
-			end 
-		  redirect_to root_path, :notice => @notice
+				@notice = "User creation FAILED"
+			end
+		 redirect_to root_path, :notice => @notice
 	end
 
 
 	private
-	def user_params
-		params.require(:user).permit(:email,
-																 :username,
-																 :first_name,
-																 :last_name,
-																 :password,
-																 :profile,
-																 :remove_profile)
-	end 
+		def user_params
+			params.require(:user).permit(:email,
+																	 :username,
+																	 :first_name,
+																	 :last_name,
+																	 :password,
+																	 :profile,
+																	 :remove_profile)
+		end
 end
