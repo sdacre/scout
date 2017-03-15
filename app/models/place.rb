e9d76cedab5916b40db8751eec0a70cae7112592
Melanie Yeoh <melanieyeoh@Melanies-MacBook-Air.local>
2017-03-15 12:16:45 +0800

    seed working

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ApplicationRecord
   mount_uploaders :image, ImageUploader
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy
   include PgSearch
   #  multisearchable against: [:name, :city, :country, :description]
 
