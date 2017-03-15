18a4e81098f62dea65e87b3b3053521fc457e6a5
taoffke <taoffke@gmail.com>
2017-03-15 16:54:12 +0800

    added new js for places#show, reviews, not working yet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery-ui/widgets/autocomplete
 //= require jquery_ujs
 //= require jquery.raty
+//= require places
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
@@ -22,3 +23,47 @@
 $(document).on('turbolinks:load', function() {
    $( '.item:first' ).addClass( 'active');
 });
+
+$(document).ready(function(){
+  $(".reviews").each(function(){
+    var This = $(this) ;
+    var Nums = This.find(".panel").size() ;
+    This.find(".panel:first").addClass("PanelAct");
+    This.append("<div class='control'></div>") ;
+    This.find(".panel").not(".PanelAct")
+      .css("left","100%")
+    for ( i=0 ; i<Nums ; i++) {
+      This.find(".control").append("<span></span>") ;
+    }
+    This.find(".control span:eq(0)").addClass("ContActive");
+    
+    This.find(".control span").click(Reviews);
+    
+    function Reviews(){
+      var loc = $(this).index();
+      var ActivLoc = This.find(".ContActive").index();
+
+      $(this).addClass("ContActive")
+        .siblings().removeAttr("class");
+      
+      if ( loc > ActivLoc ) {
+        var Dire = '100%'
+        var IDire = '-100%'
+      }
+      if ( loc < ActivLoc ) {
+        var Dire = '-100%'
+        var IDire = '100%'
+      }
+
+      This.find(".panel").not(".PanelAct")
+      .css("left",Dire) ;
+      This.find(".panel:eq("+loc+")")
+      .animate({'left':'0'},speed)
+      .addClass("PanelAct")
+      .siblings(".PanelAct")
+      .removeClass("PanelAct")
+      .animate({'left':IDire},speed);
+    }
+  });
+});
+  
